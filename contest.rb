1d443cc6acdf7975f2064962d68e176445127431
igor-kazakevich <igor.kazakevich79@gmail.com>
2016-01-01 02:21:52 +0300

    complited app!

--- contest.rb
+++ contest.rb
@@ -1,21 +1,69 @@
 require 'net/http'
 require 'cgi'
 
-class Contest
+require "./finder.rb"
 
-  def call(env)
-    puts CGI::parse(env["QUERY_STRING"])
-    return [200, {'Content-Type'=>'text/plain'}, ["Answer send!"]]
+class Contest
+  def self.prepare
+    @finder = Finder.new    
   end
 
-  def sendAnswer
+  def self.call(env)
+    @params = CGI::parse(env["QUERY_STRING"])
+
+    if env["PATH_INFO"] = "/registration"
+      puts @params["token"]
+    end
+
+    @level = @params["level"].first
+    @question = @params["question"].first
+
+    puts "Request get! Begin find..."
+
+    if @level == '1'
+      sent_answer(@finder.findTitle(@question))
+    end
+
+    if @level == '2'
+      sent_answer(@finder.findWord(@question))
+    end
+
+    if @level == '3' || @level == '4'
+      answer = []
+      
+      @question.split("\\n").each do |q|
+        answer << @finder.findWord(q)
+      end
+
+      sent_answer(answer.join(","))
+    end
+
+    if @level == '5'
+      sent_answer(@finder.find_change_word(@question))
+    end
+
+    if @level == '6' || @level == '7'
+      sent_answer(@finder.findLine(@question))
+    end
+
+    if @level == '8'
+      sent_answer(@finder.findLineWithError(@question))
+    end
+
+    return [200, {}, [""]]
+  end
+
+  def self.sent_answer(answer)
     uri = URI("http://requestb.in/1clqg6j1")
     parameters = {
-      answer: "answer",
-      token: 'f833352191dabd2141f05ac14eb82073',
-      task_id: "task_id"
+      answer: answer,
+      token: 'token',
+      task_id: @params["id"].first
     }
 
     Net::HTTP.post_form(uri, parameters)
+
+    puts "Request sent! Answer: #{answer}"
+    puts "ID: #{@params["id"].first}"
   end
 end
---
+++ finder.rb
@@ -0,0 +1,68 @@
+require 'json'
+
+class Finder
+  def initialize
+    @letters = ["о", "а", "е", "и", "н", "т", "р", "с", "л", "в", "к", "п", "м",
+  "у", "д", "я", "ы", "ь"]
+
+    file = File.read('json_base/hash_title.json')
+    @hash_title = JSON.parse(file)
+
+    file = File.read('json_base/hash_line.json')
+    @hash_line = JSON.parse(file)
+
+    file = File.read('json_base/hash_search_word.json')
+    @hash_search_word = JSON.parse(file)
+  end
+
+  def findLine(question)
+    index = ""
+    @letters.each do |l|
+      index += question.count(l).to_s
+    end
+    return @hash_line[index]
+  end
+
+  def findLineWithError(question)
+    index = ""
+
+    @letters.each do |l|
+      index += question.count(l).to_s
+    end
+
+    @hash_line.each_key do |key|
+      difference = (key.split(//) - index.split(//)).size
+      return @hash_line[key] if difference == 2
+    end
+    return nil
+  end
+
+  def findWord(question)
+    question.gsub!(/[,'—«»;:.!?()]/, "")
+    question.gsub!(/[[:blank:]]/, "")
+    question.gsub!("%WORD%", "")
+
+    return @hash_search_word[question]
+  end
+
+  def findTitle(question)
+    index = ""
+    @letters.each do |l|
+      index += question.count(l).to_s
+    end
+
+    return @hash_title[index]
+  end
+
+  def find_change_word(question)
+    question.gsub!(/[,'—«»;:.!?()]/, "")
+    question.gsub(/[[:blank:]]/, " ").split(" ").each do |word|
+      index = question.gsub(word, "").gsub(/[[:blank:]]/, "")
+      unless @hash_search_word[index].nil?
+        return @hash_search_word[index] + "," + word
+      end
+    end
+
+  end
+
+end
