af1e0e65b1e5546a3098bdff3e30639bafd989c0
igor-kazakevich <igor.kazakevich79@gmail.com>
2016-01-11 11:50:40 +0300

    fix

--- contest.rb
+++ contest.rb
@@ -82,7 +82,7 @@ class Contest
     connection.puts "Host: pushkin.rubyroid.by\r\n"
     connection.puts "Accept: application/json\r\n"
     connection.puts "Content-Type: application/json\r\n"
-    connection.puts "Content-Length: #{send_data.bytesize}\r\n\r\n"
+    connection.puts "Content-Length: #{send_data.bytesize + 2}\r\n\r\n"
 
     connection.puts "\r\n"
     connection.puts send_data
