f232ade3eaa723595a4da791f3b867a5b9417e6b
igor-kazakevich <igor.kazakevich79@gmail.com>
2016-01-11 12:06:12 +0300

    fix

--- contest.rb
+++ contest.rb
@@ -62,9 +62,9 @@ class Contest
   end
 
   def self.sent_answer(answer)
-    # http = Curl::Easy.http_post("http://pushkin.rubyroid.by/quiz",
-    #   "answer=#{answer}&token=9b22af0964399fba3c840ae210e3009a&task_id=#{@params['id']}")
-    # puts "Response: #{http.body_str}"
+    http = Curl::Easy.http_post("http://pushkin.rubyroid.by/quiz",
+      "answer=#{answer}&token=9b22af0964399fba3c840ae210e3009a&task_id=#{@params['id']}")
+    puts "Response: #{http.body_str}"
 
     # send_data = "answer=#{answer}&token=9b22af0964399fba3c840ae210e3009a&task_id=#{@params['id']}\r\n"
 
@@ -81,21 +81,21 @@ class Contest
     # puts connection.read
     # connection.close
 
-    send_data = {'answer' => answer, 'token' => '9b22af0964399fba3c840ae210e3009a', 'task_id' => @params['id']}.to_json
+    # send_data = {'answer' => answer, 'token' => '9b22af0964399fba3c840ae210e3009a', 'task_id' => @params['id']}.to_json
 
-    connection = TCPSocket.open "pushkin.rubyroid.by", 80
+    # connection = TCPSocket.open "pushkin.rubyroid.by", 80
 
-    connection.puts "POST /quiz HTTP/1.1\r\n"
-    connection.puts "Host: pushkin.rubyroid.by\r\n"
-    connection.puts "Accept: application/json\r\n"
-    connection.puts "Connection: close\r\n"
-    connection.puts "Content-Type: application/json\r\n"
-    connection.puts "Content-Length: #{send_data.bytesize + 2}\r\n\r\n"
+    # connection.puts "POST /quiz HTTP/1.1\r\n"
+    # connection.puts "Host: pushkin.rubyroid.by\r\n"
+    # connection.puts "Accept: application/json\r\n"
+    # connection.puts "Connection: close\r\n"
+    # connection.puts "Content-Type: application/json\r\n"
+    # connection.puts "Content-Length: #{send_data.bytesize + 2}\r\n\r\n"
 
-    connection.puts "\r\n"
-    connection.puts send_data
+    # connection.puts "\r\n"
+    # connection.puts send_data
     
-    connection.close
+    # connection.close
 
     puts "Request sent! Answer: #{answer}"
     puts "ID: #{@params["id"]}"
