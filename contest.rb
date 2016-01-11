320ddfb43a6285e8642d672dc158b37ddd49982c
igor-kazakevich <igor.kazakevich79@gmail.com>
2016-01-11 11:26:56 +0300

    fix

--- contest.rb
+++ contest.rb
@@ -59,22 +59,36 @@ class Contest
     #   "answer=#{answer}&token=9b22af0964399fba3c840ae210e3009a&task_id=#{@params['id']}")
     # puts "Response: #{http.body_str}"
 
-    send_data = "answer=#{answer}&token=9b22af0964399fba3c840ae210e3009a&task_id=#{@params['id']}\r\n"
+    # send_data = "answer=#{answer}&token=9b22af0964399fba3c840ae210e3009a&task_id=#{@params['id']}\r\n"
+
+    # connection = TCPSocket.open "pushkin.rubyroid.by", 80
+
+    # connection.puts "POST /quiz HTTP/1.1\r\n"
+    # connection.puts "Host: pushkin.rubyroid.by\r\n"
+    # connection.puts "Connection: close\r\n"
+    # connection.puts "Content-Type: application/x-www-form-urlencoded\r\n"
+    # connection.puts "Content-Length: #{send_data.bytesize}\r\n\r\n"
+
+    # connection.puts "\r\n"
+    # connection.puts send_data
+    # puts connection.read
+    # connection.close
+
+    send_data = {'answer' => answer, 'token' => '9b22af0964399fba3c840ae210e3009a', 'task_id' => @params['id']}.to_json
 
     connection = TCPSocket.open "pushkin.rubyroid.by", 80
 
     connection.puts "POST /quiz HTTP/1.1\r\n"
     connection.puts "Host: pushkin.rubyroid.by\r\n"
     connection.puts "Connection: close\r\n"
-    connection.puts "Content-Type: application/x-www-form-urlencoded\r\n"
-    connection.puts "Content-Length: #{send_data.bytesize}\r\n\r\n"
+    connection.puts "Accept: application/json\r\n"
+    connection.puts "Content-Type: application/json\r\n"
+    connection.puts "Content-Length: #{send_data.bytesize + 2}\r\n\r\n"
 
     connection.puts "\r\n"
     connection.puts send_data
     puts connection.read
     connection.close
-
-
     puts "Request sent! Answer: #{answer}"
     puts "ID: #{@params["id"]}"
   end
