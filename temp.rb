d986f8e1be805e78f8aaa08dfae3a934a3808c3e
igor-kazakevich <igor.kazakevich79@gmail.com>
2016-01-11 16:24:49 +0300

    fix

--- temp.rb
+++ temp.rb
@@ -1,6 +1,6 @@
 require 'socket'
 
-server = TCPServer.new 80 # Server bound to port 2000
+server = TCPServer.new ENV['PORT'] # Server bound to port 2000
 
 loop do
   client = server.accept    # Wait for a client to connect
